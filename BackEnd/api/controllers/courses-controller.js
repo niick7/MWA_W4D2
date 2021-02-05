abd05ee1087ce10cabf5039afa167587ef602f45
Nhan Vo <niick7@gmail.com>
2021-02-04 19:29:06 -0600

    First commit

--- BackEnd/api/controllers/courses-controller.js
+++ BackEnd/api/controllers/courses-controller.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+const Course = mongoose.model("Course");
+
 module.exports.register = function(req, res){
   
+}
+
+module.exports.getCourses = function(req, res){
+
 }
